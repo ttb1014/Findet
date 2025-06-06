821c06559ece1cb57dc4ac36863e6d06e1819293
ttb1014 <www.doc.ru.dd@gmail.com>
2025-06-06 18:47:29 +0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Findet"
+include(":app")
+include(":core:model")
+include(":core:mock")
+include(":core:design-system")
+include(":core:ui")
