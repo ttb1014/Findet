be5fa6aacc02b2075805149013fd7fd7f2234a2c
ttb1014 <www.doc.ru.dd@gmail.com>
2025-06-20 00:58:51 +0500

    Поправил загрузочное колесо

--- core/presentation/ui/src/main/kotlin/ru/ttb220/presentation/ui/component/LoadingWheel.kt
+++ core/presentation/ui/src/main/kotlin/ru/ttb220/presentation/ui/component/LoadingWheel.kt
@@ -1,104 +1,78 @@
 package ru.ttb220.presentation.ui.component
 
-import androidx.compose.animation.animateColor
-import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.FastOutSlowInEasing
-import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.RepeatMode
-import androidx.compose.animation.core.StartOffset
 import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.keyframes
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.graphics.drawscope.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.launch
+import ru.ttb220.presentation.ui.theme.FindetTheme
+import ru.ttb220.presentation.ui.theme.GreenHighlight
 
-private const val ROTATION_TIME = 12000
-private const val NUM_OF_LINES = 12
+private const val ROTATION_TIME = 5_000
 
 @Composable
 fun LoadingWheel(
-    modifier: Modifier = Modifier
+    modifier: Modifier,
 ) {
-    val infiniteTransition = rememberInfiniteTransition()
+    val color = MaterialTheme.colorScheme.onSurface
 
-    val startValue = 1F
-    val floatAnimValues = (0 until NUM_OF_LINES).map { remember { Animatable(startValue) } }
-    LaunchedEffect(floatAnimValues) {
-        (0 until NUM_OF_LINES).map { index ->
-            launch {
-                floatAnimValues[index].animateTo(
-                    targetValue = 0F,
-                    animationSpec = tween(
-                        durationMillis = 100,
-                        easing = FastOutSlowInEasing,
-                        delayMillis = 40 * index,
-                    ),
-                )
-            }
-        }
-    }
+    val infiniteTransition = rememberInfiniteTransition(label = "infinite")
 
-    val rotationAnim by infiniteTransition.animateFloat(
-        initialValue = 0F,
+    val rotationAnimation by infiniteTransition.animateFloat(
+        initialValue = 0f,
         targetValue = 360F,
         animationSpec = infiniteRepeatable(
-            animation = tween(durationMillis = ROTATION_TIME, easing = LinearEasing),
-        ),
-    )
-
-    val baseLineColor = MaterialTheme.colorScheme.onBackground
-    val progressLineColor = MaterialTheme.colorScheme.inversePrimary
-
-    val colorAnimValues = (0 until NUM_OF_LINES).map { index ->
-        infiniteTransition.animateColor(
-            initialValue = baseLineColor,
-            targetValue = baseLineColor,
-            animationSpec = infiniteRepeatable(
-                animation = keyframes {
-                    durationMillis = ROTATION_TIME / 2
-                    progressLineColor at ROTATION_TIME / NUM_OF_LINES / 2 using LinearEasing
-                    baseLineColor at ROTATION_TIME / NUM_OF_LINES using LinearEasing
-                },
-                repeatMode = RepeatMode.Restart,
-                initialStartOffset = StartOffset(ROTATION_TIME / NUM_OF_LINES / 2 * index),
+            animation = tween(
+                durationMillis = ROTATION_TIME,
+                easing = FastOutSlowInEasing
             ),
-        )
-    }
+            repeatMode = RepeatMode.Restart
+        ),
+        label = "AnimationPreview"
+    )
 
     Canvas(
         modifier = modifier
-            .size(48.dp)
-            .padding(8.dp)
-            .graphicsLayer { rotationZ = rotationAnim }
-            .testTag("loadingWheel"),
+            .padding(20.dp)
+            .graphicsLayer { rotationZ = rotationAnimation * 2f }
     ) {
-        repeat(NUM_OF_LINES) { index ->
-            rotate(degrees = index * 30f) {
-                drawLine(
-                    color = colorAnimValues[index].value,
-                    alpha = if (floatAnimValues[index].value < 1f) 1f else 0f,
-                    strokeWidth = 4F,
-                    cap = StrokeCap.Round,
-                    start = Offset(size.width / 2, size.height / 4),
-                    end = Offset(size.width / 2, floatAnimValues[index].value * size.height / 4),
-                )
-            }
+        drawArc(
+            color = color,
+            startAngle = -90f,
+            sweepAngle = rotationAnimation,
+            useCenter = false,
+            style = Stroke(40f)
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun LaodingWheelPreview() {
+    FindetTheme {
+        Box(
+            Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            LoadingWheel(Modifier.size(160.dp))
         }
     }
 }
